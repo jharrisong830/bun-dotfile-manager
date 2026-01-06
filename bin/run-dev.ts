f36f9626c63bef4167fee627e979f149fd6e5f08
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-01-06 15:17:14 -0500

    development runner

--- bin/run-dev.ts
+++ bin/run-dev.ts
@@ -1,7 +1,11 @@
 import { $ } from "bun";
 
+import argparse from "../src/util/argparse";
 import { getVersionNumber } from "./common";
 
+
+const args = argparse().join(" ");
+console.log(args);
 
 const platform = process.platform;
 if (!["win32", "linux", "darwin"].includes(platform)) {
@@ -13,4 +17,6 @@ const version = await getVersionNumber();
 const buildTime = Date.now().toString();
 const commitHash = await $`git rev-parse HEAD`.text();
 
-await $`bun --define PLATFORM=\"'${platform}'\" --define VERSION=\"'${version}'\" --define BUILD_TIME=\"'${buildTime}'\" --define COMMIT_HASH=\"'${commitHash.trim()}'\" index.ts`;
+console.log(`Running development version ${version} (${commitHash.trim()}) for platform ${platform}`);
+
+await $`bun --define PLATFORM=\"'${platform}'\" --define VERSION=\"'${version}'\" --define BUILD_TIME=\"'${buildTime}'\" --define COMMIT_HASH=\"'${commitHash.trim()}'\" index.ts ${args}`.nothrow();
