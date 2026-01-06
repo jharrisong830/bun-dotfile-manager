6fd18568aa2673f4d06289964aa3149642972027
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-01-06 16:42:41 -0500

    dotfile marker type and functions, to be used when reading files

---
+++ src/fs/dotfileMarkers.ts
@@ -0,0 +1,29 @@
+import { YAML } from "bun";
+
+type DotfileMarker = {
+    name: string; // name of the file
+    location: string; // where the file should be symlinked to
+}
+
+
+const markerObjToYAML = (marker: DotfileMarker): string => {
+    return YAML.stringify(marker, null, 4);
+};
+
+const YAMLDocumentToMarkerArr = (yamlString: string): Array<DotfileMarker> => {
+    const obj = YAML.parse(yamlString) as Array<Record<string, unknown>>;
+    
+    for (const item of obj) {
+        if (!Object.keys(item).includes("name") || !Object.keys(item).includes("location")) {
+            throw new Error("Invalid dotfile markers file: missing 'name' or 'location' key");
+        } 
+        if (typeof item["name"] !== "string" || typeof item["location"] !== "string") {
+            throw new Error("Invalid dotfile markers file: 'name' and 'location' must be strings");
+        }
+        if (Object.keys(item).length !== 2) {
+            throw new Error("Invalid dotfile markers file: unexpected keys present");
+        }
+    }
+
+    return obj as Array<DotfileMarker>;
+};
