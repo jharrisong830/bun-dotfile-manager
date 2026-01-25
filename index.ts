02a9e69f19f21cbfa0ec109493cf0e0ded0ea5f4
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-01-24 20:12:34 -0500

    more organized command handling

--- index.ts
+++ index.ts
@@ -13,37 +13,71 @@ const configPath = util.formatString(
 const args = argparse();
 const command = args[0];
 
-switch (command) {
-    case "init":
-        const doesExist = await configuration.doesConfigExist(configPath);
-        if (doesExist) {
-            console.error(`Configuration file already exists at ${configPath}`);
-            process.exit(1);
+type CommandName = 
+    "init" | 
+    "get-config" | 
+    "set-config" | 
+    "version" | 
+    "relink";
+
+type CommandHandler = {
+    helptext: string;
+    handler: () => Promise<void>;
+};
+
+const commandHandlers: Record<CommandName, CommandHandler> = {
+    "init": {
+        helptext: "",
+        handler: async () => {
+            const doesExist = await configuration.doesConfigExist(configPath);
+            if (doesExist) {
+                console.error(`Configuration file already exists at ${configPath}`);
+                process.exit(1);
+            }
+            await configuration.initializeConfigFile(configPath);
         }
-        await configuration.initializeConfigFile(configPath);
-        break;
-    case "get-config":
-        console.log(await configuration.readAndFormatConfig(configPath));
-        break;
-    case "set-config":
-        const dotfileRepoPath = args[1];
-        if (!dotfileRepoPath) {
-            console.error("Please provide a path for 'set-config' command.");
-            process.exit(1);
+    },
+    "get-config": {
+        helptext: "",
+        handler: async () => {
+            console.log(await configuration.readAndFormatConfig(configPath));
         }
-        await configuration.setConfig(configPath, dotfileRepoPath);
-        break;
-    case "version":
-        console.log(`bun-dotfile-manager version ${VERSION}\nbuilt on ${BUILD_TIME} from ${COMMIT_HASH}\nfor platform ${PLATFORM}`);
-        break;
-    case "relink":
-        const config = await configuration.readAndFormatConfig(configPath);
-        const markers = await dotfileMarkers.getAllDotfileMarkersForRepository(config.dotfile_repo_path);
-        for (const marker of markers) {
-            await dotfileMarkers.createSymlinkForDotfileMarker(marker);
+    },
+    "set-config": {
+        helptext: "",
+        handler: async () => {
+            const dotfileRepoPath = args[1];
+            if (!dotfileRepoPath) {
+                console.error("Please provide a path for 'set-config' command.");
+                process.exit(1);
+            }
+            await configuration.setConfig(configPath, dotfileRepoPath);
         }
-        break;
-    default:
-        console.error(`Unknown command: ${command}`);
-        process.exit(1);
+    },
+    "version": {
+        helptext: "",
+        handler: async () => {
+            console.log(`bun-dotfile-manager version ${VERSION}\nbuilt on ${BUILD_TIME} from ${COMMIT_HASH}\nfor platform ${PLATFORM}`);
+        }
+    },
+    "relink": {
+        helptext: "",
+        handler: async () => {
+            const config = await configuration.readAndFormatConfig(configPath);
+            const markers = await dotfileMarkers.getAllDotfileMarkersForRepository(config.dotfile_repo_path);
+            for (const marker of markers) {
+                await dotfileMarkers.createSymlinkForDotfileMarker(marker);
+            }
+        }
+    }
+}
+
+if (!command) {
+    console.error("Please provide a command.");
+    process.exit(1);
+} else if (command in commandHandlers) {
+    await commandHandlers[command as CommandName].handler();
+} else {
+    console.error(`Unknown command: ${command}`);
+    process.exit(1);
 }
