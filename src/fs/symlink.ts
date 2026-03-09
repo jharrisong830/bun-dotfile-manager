557998b5d7ec6fd5c4a799ba01a3abf8345bf86a
John Graham <110198683+jharrisong830@users.noreply.github.com>
2026-03-09 19:36:09 -0400

    unlink instead of delete file

--- src/fs/symlink.ts
+++ src/fs/symlink.ts
@@ -1,33 +1,32 @@
-import { symlink, lstat } from "node:fs/promises";
+import { symlink, lstat, unlink } from "node:fs/promises";
 import { createDirectoryAtPath, doesDirectoryExist } from "../util/util";
 
 const linkDotfile = async (source: string, destination: string): Promise<void> => {
     console.log(`Linking ${source} to ${destination}...`);
     await unlinkDotfile(destination);
 
-    const file = Bun.file(destination);
     const parent = destination.split("/").slice(0, -1).join("/");
     if (!(await doesDirectoryExist(parent))) {
         console.log(`Creating parent directory at ${parent}...`);
         await createDirectoryAtPath(parent);
     }
-    if (!(await file.exists())) {
+    try {
         await symlink(source, destination);
         console.log("linked!");
-    } else {
+    } catch (err: any) {
+        if (err.code !== "EEXIST") throw err;
         console.log(`${destination} already exists. Skipping link creation.`);
     }
 };
 
 const unlinkDotfile = async (linkPath: string): Promise<void> => {
     console.log(`Unlinking ${linkPath} if it exists...`);
-    const file = Bun.file(linkPath);
-    if ((await file.exists()) || (await doesDirectoryExist(linkPath))) {
+    if ((await Bun.file(linkPath).exists()) || (await doesDirectoryExist(linkPath))) {
         console.log("exists!");
         const nodefsStat = await lstat(linkPath);
         if (nodefsStat.isSymbolicLink()) {
             console.log(`Removing symlink at ${linkPath}`);
-            await file.delete();
+            await unlink(linkPath);
             console.log("removed!");
         } else {
             throw new Error(`${linkPath} exists and is not a symlink. Refusing to delete.`);
